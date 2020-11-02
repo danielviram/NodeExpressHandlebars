34f24c089ce4a5210adfefdf528f322fd41f1538
Daniel Viramontes <danielviramo@gmail.com>
2020-11-01 21:29:53 -0600

    adding more stuff

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burger (burger_name, devoured) VALUES ("Chicken Burger",false);
+INSERT INTO burger (burger_name, devoured) VALUES ("Bacon Burger",false);
+INSERT INTO burger (burger_name, devoured) VALUES ("Cheese Burger",false);
+
+SELECT * FROM burger;
+UPDATE burger SET devoured = 1 WHERE id = 1;
